d1b50d9f72b2eb02c145c5baf9c0e719ca04c67d
SixSlime <ryan.mm.taylor@gmail.com>
2026-06-10 18:58:20 -0700

    -

--- Internal/Region.cs
+++ Internal/Region.cs
@@ -3,15 +3,26 @@ namespace CVMatrix.DropOffDefense.SLib.Internal;
 using Loc;
 using Loc.Tags;
 using Util.RegionManagement;
-
+using Util.Collections;
 internal class Region : TaggableMapElement<IRegionTag>, ILocRegion
 {
     public required IReadOnlyList<Coordinates> Boundary { get; set; }
     public required IReadOnlyList<IReadOnlyList<Coordinates>> SubtractiveBoundaries { get; set; }
     IReadOnlyList<ILocCoordinates> ILocRegion.Boundary => Boundary;
 
+    private IRegionHandle<Region, Way, IPointElement>? _regionHandle = null;
+
+    private IRegionHandle<Region, Way, IPointElement> AccessRegionHandle()
+    {
+        _regionHandle ??= SourceMap.Data.RegionManager.GetRegion(this);
+        return _regionHandle;
+    }
     IReadOnlyList<IReadOnlyList<ILocCoordinates>> ILocRegion.SubtractiveBoundaries => SubtractiveBoundaries;
-    IReadOnlySet<ILocMapElement> ILocRegion.EncompassedElements => throw new NotImplementedException();
+
+    IReadOnlySet<ILocRegion> ILocRegion.EncompassesRegions => AccessRegionHandle().EncompassesRegions.CastingView<Region, ILocRegion>();
+    IReadOnlySet<ILocWay> ILocRegion.EncompassesWays => AccessRegionHandle().EncompassesLines.CastingView<Way, ILocWay>();
+    IReadOnlySet<ILocPoi> ILocRegion.EncompassesPois => new HashSet<ILocPoi>(AccessRegionHandle().EncompassesPoints.Where(x => x is Poi).Cast<ILocPoi>());
+    IReadOnlySet<ILocIntersection> ILocRegion.EncompassesIntersections => new HashSet<ILocIntersection>(AccessRegionHandle().EncompassesPoints.Where(x => x is Intersection).Cast<ILocIntersection>());
 
     protected override IElementHandle<Region> GetRegionElementHandle(RegionManager<Region, Way, IPointElement> manager)
     {
--- Loc/ILocRegion.cs
+++ Loc/ILocRegion.cs
@@ -4,5 +4,8 @@ public interface ILocRegion : ILocMapElement, ILocTaggable<Tags.IRegionTag>
 {
     public IReadOnlyList<ILocCoordinates> Boundary { get; }
     public IReadOnlyList<IReadOnlyList<ILocCoordinates>> SubtractiveBoundaries { get; }
-    public IReadOnlySet<ILocMapElement> EncompassedElements { get; }
+    public IReadOnlySet<ILocPoi> EncompassesPois { get; }
+    public IReadOnlySet<ILocRegion> EncompassesRegions { get; }
+    public IReadOnlySet<ILocWay> EncompassesWays { get; }
+    public IReadOnlySet<ILocIntersection> EncompassesIntersections { get; }
 }
